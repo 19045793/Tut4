a120d0394f8ad54b480e7a5fe9b2b64d65484a05
Samuel <SamSecondAc@gmail.com>
2022-08-21 22:00:21 +1200

    modified:   src/main/java/Tutorial4/Calc.java

--- src/main/java/Tutorial4/Calc.java
+++ src/main/java/Tutorial4/Calc.java
@@ -3,5 +3,5 @@ package Tutorial4;
 public class Calc
 {
   public int add(int a, int b){return a+b;};
-  public int subtract(int a, int b){return a+b;};
+  public int subtract(int a, int b){return a-b;};
 }
